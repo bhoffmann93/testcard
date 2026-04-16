9a67bb751f96bd2d1b55b2e144d5eaacb5d5c889
bhoffmann93 <b-hoffmann@live.de>
2026-04-16 18:10:16 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/testcard/',
   server: {
     port: '8080',
     open: 'index.html',
